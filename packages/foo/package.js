f90720ddc4a492cfe231b2b5b12d1c6f72396e1f
SEKE <sebastian.kerckhof@barco.com>
2016-07-06 09:44:42 +0200

    first commit

---
+++ packages/foo/package.js
@@ -0,0 +1,37 @@
+Package.describe({
+  summary: "Foo",
+  version:"1.0.0",
+  name: "foo",
+  documentation:null
+});
+
+//Npm.depends({
+//  'angular':'1.5.7'
+//});
+
+Package.onUse(function(api) {
+  api.versionsFrom("1.3.4.1");
+
+  api.imply([]);
+
+  api.use([
+    'meteor',
+    'urigo:static-templates@0.0.5',
+    'pbastowski:angular-babel@1.3.6',
+    'tmeasday:check-npm-versions@0.3.1'
+  ]);
+
+
+  api.mainModule('client/main.js', 'client');
+
+  // PACKAGES FOR SERVER
+  api.use([], {weak:true});
+  // UNORDERED DEPENDENCIES (to solve
+  api.use([], {unordered:true});
+
+  //EXPORT VARIABLES
+  api.export([]);
+
+});
+
+Package.onTest(function (api) {});
